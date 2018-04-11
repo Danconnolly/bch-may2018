8dd50ad74c1ffff5fe4f859e6734be10b989195e
Daniel Connolly <daniel@dconnolly.com>
2018-04-11 23:33:13 +0200

    rewrite of op_cat op_dup tests

--- opcat_test.sh
+++ opcat_test.sh
@@ -1,137 +1,86 @@
 #!/bin/bash
-export LC_ALL=en_US.UTF-8
 
 # to test the op_cat opcode, to ensure that it does not cause memory issues
-
+# DO NOT RUN THIS ON MAINNET - it loses funds and also exposes funds to theft
 # script based on https://github.com/matiu/opcode-tests/blob/master/stress-opcodes.sh
-#CLI="/home/daniel/bin/bitcoin-cli -regtest "
-CLI="docker exec --user bitcoin $(docker ps --filter name=bchabcmay -q) /usr/local/bin/bitcoin-cli -conf=/home/bitcoin/bitcoin.conf "
-#CLITX="/home/daniel/bin/bitcoin-tx -regtest "
-CLITX="docker exec --user bitcoin $(docker ps --filter name=bchabcmay -q) /usr/local/bin/bitcoin-tx -conf=/home/bitcoin/bitcoin.conf "
-SCRIPT="1 0 XOR DROP OP_DUP OP_HASH160 0x1455c10a3aba166ff020568d45b3d49941e92153bb OP_EQUALVERIFY OP_CHECKSIG"
-FEE=0.0001
-COUNT=1
-OUTPUTSPERTX=1
 
-ECHO="echo "
+# creates a funding tx with two outputs
+#	first output script is valid, OP_CAT top of stack up to 520 bytes
+#       second output script is invalid, OP_CAT top of stack up to 521 bytes
+# then creates a tx for each output script, returning funds
+# second tx should fail
+
+export LC_ALL=en_US.UTF-8               # remove possible locale problems
+
+CLI="/home/daniel/bin/bitcoin-cli "
+#CLI="docker exec --user bitcoin $(docker ps --filter name=bchabcmay -q) /usr/local/bin/bitcoin-cli -conf=/home/bitcoin/bitcoin.conf "
+CLITX="/home/daniel/bin/bitcoin-tx -testnet "
+#CLITX="docker exec --user bitcoin $(docker ps --filter name=bchabcmay -q) /usr/local/bin/bitcoin-tx -conf=/home/bitcoin/bitcoin.conf "
+
+# do several DUP CAT's but stay within the max size bounds
+SCRIPT1="0x400102030405060708090A0B0C0D0E0F100102030405060708090A0B0C0D0E0F200102030405060708090A0B0C0D0E0F300102030405060708090A0B0C0D0E0F40 DUP CAT DUP CAT DUP CAT 0x080102030405060708 CAT SIZE 0x020802 NUMEQUALVERIFY"
+
+# do many DUP CAT cycles, to make it just too big - will fail when spent
+SCRIPT2="0x400102030405060708090A0B0C0D0E0F100102030405060708090A0B0C0D0E0F200102030405060708090A0B0C0D0E0F300102030405060708090A0B0C0D0E0F40 DUP CAT DUP CAT DUP CAT 0x09010203040506070809 CAT 1"
+
+FEE=0.0001
 
 command -v bc >/dev/null 2>&1 || { echo >&2 "ERROR: bc not found, install with apt install bc.  Aborting."; exit 1; }
 command -v jq >/dev/null 2>&1 || { echo >&2 "ERROR: jq not found, install with apt install jq.  Aborting."; exit 1; }
 
-$ECHO "TEST CASE DESCRIPTON"
-$ECHO "  Script:$SCRIPT"
-$ECHO "  (nr opcodes):`echo $SCRIPT | wc -w`"
-$ECHO "  NR OF TXs: $COUNT"
-$ECHO "  NR OF OUTPUTS PER TXs: $OUTPUTSPERTX"
-
-function getUnspent () {
-    $ECHO " == Getting UTXOs"
-    ALL_UTXOS=`$CLI  listunspent | jq ".|=sort_by(.amount)|reverse|.[0:$COUNT]"`
-
-    COUNTER=0
-    while [  $COUNTER -lt $COUNT ]; do
-        INDEX=$((1+COUNTER))
-        UTXO=`echo $ALL_UTXOS| jq ".[-$INDEX]"`
-        OUTTX=`echo $UTXO | jq .txid | sed -e 's/^"//' -e 's/"$//'`
-	OINDEX=$(echo $UTXO | jq .vout)
-	OUTPOINT=$(echo $OUTTX:$OINDEX)
-        VALUE=`echo $UTXO | jq .amount | sed -e 's/^"//' -e 's/"$//'`
-
-	if [ $VALUE != "null" ]; then
-            UNSPENT="$UNSPENT $OUTPOINT|$VALUE"
-	fi
-
-        let COUNTER=COUNTER+1 
-    done
-}
-
-function createTxs () {
-    $ECHO " == Creating TXs"
-    for UTXO in $UNSPENT; do
-        O=`echo $UTXO| cut -d "|" -f 1`;
-        V=`echo $UTXO| cut -d "|" -f 2`;
-
-        VALUE=`echo "($V - $FEE)/$OUTPUTSPERTX" | bc -l`
-        VALUE=`printf %.8f $VALUE`
-
-        COUNTER=0
-        TXCMD="$CLITX -create in=$O"
-        while [  $COUNTER -lt $OUTPUTSPERTX ]; do
-          TXCMD="$TXCMD outscript=$VALUE:\"$SCRIPT\""
-          let COUNTER=COUNTER+1 
-        done
-
-	$ECHO "txcmd = $TXCMD"
-        TX1=`eval $TXCMD`
-	$ECHO "result = $TX1"
-	RESULT=$($CLI signrawtransaction $TX1)
-	COMPLETE=$(echo $RESULT | jq .complete)
-	if [ $COMPLETE == "false" ]; then
-	    $ECHO "ERROR: signing transaction failed, result=$RESULT"
-	    exit 1
-	fi
-	TX=$(echo $RESULT | jq .hex  | sed -e 's/^"//' -e 's/"$//')
-	$ECHO "signed = $TX"
-
-        TXS="$TXS $TX|$V"
-    done
-    $ECHO "all txs = $TXS"
-}
-
-function sendTxs () {
-    $ECHO " == Sending TXs"
-    for TXVALUE in $TXS; do
-        TX=`echo $TXVALUE| cut -d "|" -f 1`;
-        V=`echo $TXVALUE| cut -d "|" -f 2`;
-
-        TXID=`$CLI sendrawtransaction $TX`
-        TXIDS="$TXIDS $TXID|$V"
-    done
-    $ECHO "tx sent, txids=$TXIDS"
-}
-
-
-function createSpendTxs () {
-    $ECHO " == Creating Spend TXs"
-    for TXIDVALUE in $TXIDS ; do
-        TXID=`echo $TXIDVALUE| cut -d "|" -f 1`;
-        V=`echo $TXIDVALUE| cut -d "|" -f 2`;
-        VALUE=`echo $VALUE - $FEE | bc`
-
-        COUNTER=0
-        TXCMD="$CLITX -create"
-        while [  $COUNTER -lt $OUTPUTSPERTX ]; do
-            TXCMD="$TXCMD  in=$TXID:$COUNTER"
-          let COUNTER=COUNTER+1 
-        done
-
-        TXCMD="$TXCMD outaddr=$VALUE:$RECOVERADDR"
-        TX1=`eval $TXCMD`
-
-        STXS="$STXS $TX"
-    done
-}
-
-function sendSpendTxs () {
-    $ECHO " ## Sending Spend TXs, USING <SCRIPT> ## TX length:$LENGTH b"
-    for TX in $STXS; do
-        TXID=`$CLI sendrawtransaction $TX`
-	$ECHO "spendtx sent, txid=$TXID"
-    done
-}
-
 RECOVERADDR=$($CLI getnewaddress | cut -d ":" -f 2)
-$ECHO "recovery address=$RECOVERADDR"
+echo "recovery address=$RECOVERADDR"
 
-getUnspent
-$ECHO "unspent tx = $UNSPENT"
+ALL_UTXOS=`$CLI  listunspent | jq ".|=sort_by(.amount)|reverse|.[0:1]"`
+if [ "$ALL_UTXOS" == "[]" ]; then
+    echo "no utxo's found."
+    exit 1
+fi
+UTXO=`echo $ALL_UTXOS| jq ".[-1]"`
+OUTTX=`echo $UTXO | jq .txid | sed -e 's/^"//' -e 's/"$//'`
+OINDEX=$(echo $UTXO | jq .vout)
+OUTPOINT=$(echo $OUTTX:$OINDEX)
+VALUE=`echo $UTXO | jq .amount | sed -e 's/^"//' -e 's/"$//'`
+echo "found utxo $OUTPOINT, value $VALUE"
 
-createTxs
-#time sendTxs
+HIGHVAL=$(echo "$VALUE - ($FEE * 3)" | bc -l)
+HIGHVAL=$(printf %.8f $HIGHVAL)
+LOWVAL=$(echo "$FEE * 2" | bc -l)
+LOWVAL=$(printf %.8f $LOWVAL)
 
-#createSpendTxs
-#time sendSpendTxs
+TXCMD="$CLITX -create in=$OUTPOINT outscript=$HIGHVAL:\"$SCRIPT1\" outscript=$LOWVAL:\"$SCRIPT2\""
+#echo $TXCMD
 
+FUNDTXRAW=$(eval $TXCMD)
+FTXRAWSIGN=$($CLI signrawtransaction $FUNDTXRAW)
+COMPLETE=$(echo $FTXRAWSIGN | jq .complete)
+if [ $COMPLETE == "false" ]; then
+    echo "ERROR: signing transaction failed, result=$FTXRAWSIGN"
+    exit 1
+fi
+FTXRAWSIGN=$(echo $FTXRAWSIGN | jq .hex  | sed -e 's/^"//' -e 's/"$//')
+#echo "funding tx signed, raw=$FTXRAWSIGN"
+FTXID=$($CLI sendrawtransaction $FTXRAWSIGN)
+echo "funding tx sent, txid=$FTXID"
+
+# spend first output, should pass
+RETVAL=$(echo "$HIGHVAL - $FEE" | bc -l)
+RETVAL=$(printf %.8f $RETVAL)
+TXCMD="$CLITX -create in=$FTXID:0 outaddr=$RETVAL:$RECOVERADDR"
+S1RAW=$(eval $TXCMD)
+S1TXID=$($CLI sendrawtransaction $S1RAW)
+echo "sent first tx, txid=$S1TXID"
+
+# spend second output, should fail
+TXCMD="$CLITX -create in=$FTXID:1 outaddr=$FEE:$RECOVERADDR"
+S2RAW=$(eval $TXCMD)
+S2RESULT=$($CLI sendrawtransaction $S2RAW >/dev/null 2>&1)
+if [ $? == 26 ]; then
+    echo "send of second spending tx failed as expected"
+else
+    echo "send of second spending tx did not fail as expected, result=$S2RESULT"
+    exit 1
+fi
 
 
 
